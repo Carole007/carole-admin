28e07383577fcb0f2298ae11c159caa91929b7e1
carole <2282169525@qq.com>
2024-06-12 14:44:10 +0800

    upload:remove-throttle-headers.interceptor

--- server/src/common/interceptors/remove-throttle-headers.interceptor.ts
+++ server/src/common/interceptors/remove-throttle-headers.interceptor.ts
@@ -1,17 +1,18 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class RemoveThrottleHeadersInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
-      tap(() => {
-        const response = context.switchToHttp().getResponse();
-        response.removeHeader('X-RateLimit-Limit');
-        response.removeHeader('X-RateLimit-Remaining');
-        response.removeHeader('X-RateLimit-Reset');
-        response.removeHeader('Retry-After');
+      map((data) => {  
+        const response = context.switchToHttp().getResponse();  
+        response.removeHeader('X-RateLimit-Limit');  
+        response.removeHeader('X-RateLimit-Remaining');  
+        response.removeHeader('X-RateLimit-Reset');  
+        response.removeHeader('Retry-After');  
+        return data;
       }),
     );
   }
