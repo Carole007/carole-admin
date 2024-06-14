f21a4a301a6d88db0353c84fcf15b0a447a1189d
carole <2282169525@qq.com>
2024-06-14 22:45:58 +0800

    fit:baseDomain

--- server/src/common/domain/BaseDomain.ts
+++ server/src/common/domain/BaseDomain.ts
@@ -1,14 +1,25 @@
 import { ApiProperty } from "@nestjs/swagger"
+import { IsOptional, IsString } from "class-validator"
 
 export class BaseDomain {
-  @ApiProperty({description:"创建者"})
+  @ApiProperty({ description: "创建者" })
+  @IsString()
+  @IsOptional()
   createBy?: string
-  @ApiProperty({description:"创建时间"})
+  @ApiProperty({ description: "创建时间" })
+  @IsString()
+  @IsOptional()
   createTime?: string
-  @ApiProperty({description:"更新者"})
+  @ApiProperty({ description: "更新者" })
+  @IsString()
+  @IsOptional()
   updateBy?: string
-  @ApiProperty({description:"更新时间"})
+  @ApiProperty({ description: "更新时间" })
+  @IsString()
+  @IsOptional()
   updateTime?: string
-  @ApiProperty({description:"备注"})
+  @ApiProperty({ description: "备注" })
+  @IsString()
+  @IsOptional()
   remark?:string
 }
